d5e32b600abf48b08afaba36f959e01ef1dc9d52
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-10-05 13:59:09 +0100

    lab complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.destroy_all
+Student.create!(first_name: "Dwayne", last_name: "Johnny", grade: 3)
